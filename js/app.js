2dfeb900fb3c0c3ea5bdc4bdec9bc7cf9e523012
catdude2000 <michael.g.pace@gmail.com>
2022-04-19 16:38:37 -0500

    added css file and linked, corrected answer capitilization, cleaned code, removed redundant function call

--- js/app.js
+++ js/app.js
@@ -3,24 +3,19 @@
 console.log('app.js file is connected');
 
 // array of answers
-const magicAnswers = ['Yes', 'no', 'maybe'];
+const magicAnswers = ['Yes', 'No', 'Maybe'];
 
+// links button to shakebutton variable
 let shakeButton = document.getElementById('shake');
-// let ballAnswer = document.getElementById('answerSpace');
 
-// assigns randanswer func to button click
+// assigns randAnswer func to button click through linked shakebutton
 shakeButton.addEventListener('click', randAnswer);
 
+// function to randomly generate an answer and display it
 function randAnswer (){
-
-  console.log('button was clicked');
-
+  // console.log('button was clicked');
   let ballAnswerIndexPos = Math.floor(Math.random() * magicAnswers.length);
-
-  console.log(ballAnswerIndexPos, 'generated answer index num');
-
   let actualAnswer = magicAnswers[ballAnswerIndexPos];
-
   console.log(actualAnswer, 'actual answer');
 
   let ul = document.getElementById('answerSpace');
@@ -28,6 +23,4 @@ function randAnswer (){
   let li = document.createElement('li');
   li.textContent = actualAnswer;
   ul.appendChild(li);
-
-
 }
