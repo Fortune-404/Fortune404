fc126979dc29133ff1bffe29c094f7f6655df433
Michael Pace <michael.g.pace@gmail.com>
2022-04-19 14:09:16 -0500

    added shake button, started js llogic

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('app.js file is connected');
+
+const magicAnswers = ['Yes', 'no', 'maybe'];
+
+let shakeButton = document.getElementById('shake');
+let ballAnswer = document.getElementById('answerSpace');
+
+addEventListener('click', randAnswer);
+
+function randAnswer (event){
+
+}
