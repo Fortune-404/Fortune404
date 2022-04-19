ef736c9473b422411de3cdeb265de51c687903cc
catdude2000 <michael.g.pace@gmail.com>
2022-04-19 15:37:21 -0500

    rand answer function works on button click and is linked to html li but no display

--- js/app.js
+++ js/app.js
@@ -2,13 +2,22 @@
 
 console.log('app.js file is connected');
 
+// array of answers
 const magicAnswers = ['Yes', 'no', 'maybe'];
 
 let shakeButton = document.getElementById('shake');
-let ballAnswer = document.getElementById('answerSpace');
+// let ballAnswer = document.getElementById('answerSpace');
 
-addEventListener('click', randAnswer);
+// assigns randanswer func to button click
+shakeButton.addEventListener('click', randAnswer);
 
-function randAnswer (event){
+function randAnswer (){
+  let ballAnswerIndexPos = Math.floor(Math.random() * magicAnswers.length);
+  console.log(ballAnswerIndexPos, 'generated answer');
+  let ul = document.getElementById('answerSpace');
+  ul.innerHTML = '';
+  let li = document.createElement('li');
+  li.textcontent = ballAnswerIndexPos;
+
 
 }
