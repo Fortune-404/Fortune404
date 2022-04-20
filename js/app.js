4cd59bd25515df5879ff2c995bf2b60cfd88f30d
Michael Pace <michael.g.pace@gmail.com>
2022-04-20 13:32:06 -0500

    ball now shakes on start, stops and fades in answer on button click, new button to start over by reload

--- js/app.js
+++ js/app.js
@@ -7,13 +7,31 @@ const magicAnswers = ['Yes', 'No', 'Maybe'];
 
 // links button to shakebutton variable
 let shakeButton = document.getElementById('shake');
+// let answerButton = document.getElementById('backBall');
 
 // assigns randAnswer func to button click through linked shakebutton
 shakeButton.addEventListener('click', randAnswer);
 
+// answerButton.addEventListener('click', randAnswer);
+
+
+let reshakeButton = document.getElementById('reshake');
+reshakeButton.addEventListener('click', shakeAgain);
+
 // function to randomly generate an answer and display it
 function randAnswer (){
   // console.log('button was clicked');
+//   let removeClass = document.getElementById('ballClass')
+// removeClass.classList.remove('back')
+  // const shaker = document.getElementById('backBall')
+
+  let backBall = document.getElementById('backBall');
+  backBall.removeAttribute('id');
+
+  // const img =  document.querySelector('img');
+  // img.classList.remove('shake');
+
+
   let ballAnswerIndexPos = Math.floor(Math.random() * magicAnswers.length);
   let actualAnswer = magicAnswers[ballAnswerIndexPos];
   console.log(actualAnswer, 'actual answer');
@@ -23,4 +41,9 @@ function randAnswer (){
   let li = document.createElement('li');
   li.textContent = actualAnswer;
   ul.appendChild(li);
+}
+function shakeAgain(){
+  let secondBall = document.getElementsByClassName('backBall');
+  // backBall.classList.remove('stop');
+  window.location.reload();
 }
