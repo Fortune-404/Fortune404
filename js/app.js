8a8c889f8c3f6056d484348651e9e1cd305dbb5e
Michael Pace <michael.g.pace@gmail.com>
2022-04-22 10:46:09 -0500

    questions have space vefore answer, cleaned js

--- js/app.js
+++ js/app.js
@@ -2,34 +2,22 @@
 
 console.log('app.js file is connected');
 
-// Things I did
-// 1. reorganize with globals on top and then functions then event listeners
-// 2. Add if logic for null state of local storage
-// 3. took local storage array into for loop to repopulate our questions array.
-//4. Added an array for the user fortunes so you can code the question with the response, we can do this together in the morning.
-//5. moved the local storage functionality to its own array. that we call from the randAnswer
-
 let questArray = [];
 let userMagicAnswers = [];
 const magicAnswers = ['Yes', 'No', 'Maybe'];
-
 // links button to shakebutton variable
 let stopButton = document.getElementById('stop');
 let reshakeButton = document.getElementById('reshake');
 let listButton = document.getElementById('showList');
 
-
-
 function shakeAgain(){
   window.location.reload();
 }
 
-
-
-
-
+// retrieves data from local storage
 let questRetrieve = localStorage.getItem('questions');
 
+// prevents null from being in array
 if(questRetrieve){
   console.log('not null local storage',questRetrieve);
   questArray = [];
@@ -39,16 +27,7 @@ if(questRetrieve){
   for(let i = 0; i < questionArrayFromLS.length; i++){
     questArray.push(questionArrayFromLS[i]);
   }
-
-  // console.log('From local storage into our for loop: ',questArray);
 }
-
-
-
-
-
-
-
 
 // function to randomly generate an answer and display it
 function randAnswer(){
@@ -57,9 +36,7 @@ function randAnswer(){
   backBall.removeAttribute('id');
   // calculates answer
   let actualAnswer = magicAnswers[Math.floor(Math.random() * magicAnswers.length)];
-  // console.log('actual answer: ', actualAnswer );
   userMagicAnswers.push(actualAnswer);
-  // console.log('match the fortune to the questions' ,userMagicAnswers);
   // gives access to answer space in ball
   let ul = document.getElementById('answerSpace');
   ul.innerHTML = '';
@@ -69,50 +46,23 @@ function randAnswer(){
   ul.appendChild(li);
   // puts text entered into variable questions
   let questions = document.getElementById('question').value;
-  // pushes text string to array
-
+  // adds questions to corresponding answer in new array
   let fullArray = questions.concat(' ', userMagicAnswers);
   console.log(fullArray, 'fullarray');
-
-  // fullArray.join();
-
+  // pushes question linked with answers in string to array for storage
   questArray.push(fullArray);
 
-
   setLocalStorage(questArray);
-
-
 }
 
-
-function setLocalStorage(questionArray){
-  console.log('array of asked questions',questionArray);
-
+function setLocalStorage(){
   const questionString = JSON.stringify(questArray);
-  console.log('question stringify', questionString);
   localStorage.setItem('questions', questionString);
 }
-
-
-
-
 
 function getOldList(){
   let oldList = JSON.parse(localStorage.getItem('questions'));
   // gives access to answer space in ball
-
-
-  console.log(oldList, 'old list');
-
-  // for (let i = 0; i < oldList.length; i++) {
-  //   let oldList[i]
-  // }
-  // console.log(userMagicAnswers, 'userMagicAnswers');
-  // let fullList = userMagicAnswers.concat(oldList);
-  // console.log(fullList, 'full list');
-
-
-
   let ul = document.getElementById('questSpace');
   ul.innerHTML = '';
   let li = document.createElement('li');
@@ -120,16 +70,7 @@ function getOldList(){
   li.textContent = oldList;
   ul.appendChild(li);
   // puts text entered into variable questions
-  // let questions = document.getElementById('question').value;
 }
-
-
-
-
-
-
-
-
 
 // assigns randAnswer func to button click through linked shakebutton
 stopButton.addEventListener('click', randAnswer);
